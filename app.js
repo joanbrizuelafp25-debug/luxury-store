3f3e055095036df6b01a7ab69054ef4ee7aa9f38
joanbrizuelafp25-debug <joanbrizuelafp25@ibf.cat>
2026-02-26 19:44:23 +0100

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.btn-comprar').forEach(boton => {
-    boton.addEventListener('click', () => {
-        const producto = boton.dataset.producto;
-        const mensaje = encodeURIComponent(`Hola, quiero comprar: ${producto}`);
-        window.open(`https://wa.me/34695957300?text=${mensaje}`, "_blank");
-    });
-});
